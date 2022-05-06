ca17440c5fdfa7a3a4dcedab744dc3b22f87c280
ozenerdem <ozenerdem98@gmail.com>
2022-05-06 18:10:57 +0300

    Contact page was created.

---
+++ app/controller/api/contact.php
@@ -0,0 +1,27 @@
+<?php
+
+if($data = form_control('phone')){
+    if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
+        $json['error'] = 'Lütfen geçerli bir e-posta adresi giriniz.';
+    }else{
+
+    $query = $db->insert('contact')
+        ->set([
+           'contact_name' => $data['name'],
+           'contact_email' => $data['email'],
+           'contact_phone' => $data['phone'],
+           'contact_subject' => $data['subject'],
+           'contact_message' => $data['message']
+        ]);
+
+    if($query){
+        $json['success'] = 'Mesajınız bize ulaştı, teşekkür ederiz';
+    }else{
+        $json['error'] = 'Bir sorun oluştu, mesajınız gönderilemedi!';
+    }
+    
+    }
+
+}else{
+    $json['error'] = 'Lütfen tüm alanları doldurup tekrar deneyin.';
+}
--- app/view/easy/static/header.php
+++ app/view/easy/static/header.php
@@ -15,9 +15,8 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-            crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
             integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
             crossorigin="anonymous"></script>
@@ -25,8 +24,11 @@
             integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
             crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="<?= public_url('styles/main.css') ?>">
-
+<!--    <link rel="stylesheet" href="--><?//= public_url('styles/main.css') ?><!--">-->
+    <script>
+        var api_url = '<?=site_url('api')?>';
+    </script>
+    <script src="<?=public_url('scripts/api.js')?>"></script>
 </head>
 <body>
 
@@ -61,7 +63,7 @@
                     <a class="nav-link" href="#">Hakkımda</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">İletişim</a>
+                    <a class="nav-link" href="<?=site_url('contact')?>">İletişim</a>
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0 mr-3">
